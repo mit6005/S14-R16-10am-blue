b96c8bcfbe0ae694586847998f201bfba07fe443
Danny Callahan <dc11@mit.edu>
2014-05-01 10:52:12 -0400

    no getter

--- src/music/Concat.java
+++ src/music/Concat.java
@@ -31,10 +31,6 @@ public class Concat implements Music {
         return first;
     }
     
-    public Music second() {
-    	return second;
-    }
-    
     /**
      * @return duration of this concatenation
      */
