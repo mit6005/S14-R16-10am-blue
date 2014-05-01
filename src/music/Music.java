0544edae4dd2e315675fd58442192cef1b0d1c89
Ben Bitdiddle <benbit-6.005@mit.edu>
2014-05-01 08:59:18 -0400

    Initial version of music datatype

---
+++ src/music/Music.java
@@ -0,0 +1,11 @@
+package music;
+
+/**
+ * Music represents a piece of music played by multiple instruments.
+ */
+public interface Music {
+    /**
+     * @return total duration of this piece
+     */
+    double duration();
+}
